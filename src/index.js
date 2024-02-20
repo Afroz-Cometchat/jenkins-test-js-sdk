47979fa3ad854967c8d958b0b54e8157b38a85fc
Afroz.Cometchat <afroz.khan@cometchat.com>
2024-02-20 13:23:13 +0530

    jenkins build test

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { CometChat } from '@cometchat/chat-sdk-javascript'
+
+(async () => {
+  let appID = "2498973d0fc9cbe6";
+  let region = "in";
+  let appSetting = new CometChat.AppSettingsBuilder()
+    .subscribePresenceForAllUsers()
+    .setRegion(region)
+    .autoEstablishSocketConnection(true)
+    .build();
+  CometChat.init(appID, appSetting).then(
+    () => {
+      console.log("Initialization completed successfully");
+    }, error => {
+      console.log("Initialization failed with error:", error);
+    }
+  );
+})();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
